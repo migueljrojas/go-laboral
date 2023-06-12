2dd00cc21101b908c62383332f6d6d42121a35c0
Mike <migueljrojas@gmail.com>
2023-06-12 17:19:26 -0300

    testing deploy 2

--- dist/client/assets/chunks/chunk-3818d69e.js
+++ dist/client/assets/chunks/chunk-3818d69e.js
@@ -1,1 +1,1 @@
-const s="/assets/148635659_l_normal_none.jpg",n="/assets/198904724_l_normal_none.jpg";export{s as S,n as a};
+const s="assets/148635659_l_normal_none.jpg",n="assets/198904724_l_normal_none.jpg";export{s as S,n as a};
